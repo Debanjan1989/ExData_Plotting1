d2ec1695abf965890faa67c43538f24ee0e4c4bf
Debanjan1989 <53031951+Debanjan1989@users.noreply.github.com>
2019-09-07 23:45:48 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -11,9 +11,9 @@ power_consumption$Date <- strptime(paste(power_consumption$Date, power_consumpti
 png("plot3.png",width = 480, height = 480)
 
 with(power_consumption,plot(Date,Sub_metering_1,type="n",ylab="Energy sub metering" , xlab = ""))
-points(power_consumption$Date,power_consumption$Sub_metering_1,type = "l",col="green")
+points(power_consumption$Date,power_consumption$Sub_metering_1,type = "l",col="black")
 points(power_consumption$Date,power_consumption$Sub_metering_2,type = "l",col="red")
 points(power_consumption$Date,power_consumption$Sub_metering_3,type = "l",col="blue")
-legend("topright",col =c("green","red","blue"),lty=1,lwd=2,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",col =c("black","red","blue"),lty=1,lwd=2,legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
 
